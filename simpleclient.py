d22c53785fcff1b6b9fc6caa8d618fb125cbefab
ajithkumar26 <40885695+ajithkumar26@users.noreply.github.com>
2018-07-06 14:22:18 +0530

    Create simpleclient.py

---
+++ simpleclient.py
@@ -0,0 +1,7 @@
+import socket
+udp_ip_address="127.0.0.1"
+udp_port_no=5555
+Message=("hello")
+bytestosend=str.encode(Message)
+clientsock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+clientsock.sendto(bytestosend,(udp_ip_address,udp_port_no))
